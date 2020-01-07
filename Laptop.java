d4574dee6fd9a1f36de50bc600bdf5435f9e9452
DEEDin <48057278+DEEDin@users.noreply.github.com>
2020-01-07 07:35:22 -0800

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,12 @@
+public class Laptop{
+	String name="Lenovo";
+	int width= 14;
+void executes(){
+	System.out.println("HI my name is Dilip Magar. ");
+	System.out.println("Ahhh, I'm kind new in programming.");
+			}
+public static void main(String[] args){
+	Laptop l= new Laptop();
+	l.executes();
+ }
+}
